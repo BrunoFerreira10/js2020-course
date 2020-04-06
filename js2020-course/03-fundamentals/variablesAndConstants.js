a3b2717bade964e409f4d53387807462ae94ddf7
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-06 12:57:36 -0300

    15. Variables and constants

---
+++ js2020-course/03-fundamentals/variablesAndConstants.js
@@ -0,0 +1,17 @@
+var a = 3
+let b = 4
+
+var a = 30 // You can declare var again...
+b = 40 // You cannot declare let again
+
+console.log(a, b)
+
+a = 300
+d = 400
+
+console.log(a, b)
+
+const c = 5
+//c = 50 // Not work
+
+console.log(a,b,c)
