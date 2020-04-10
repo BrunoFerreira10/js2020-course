fe03f6afdfdee76075bf3ab437933d421e6d6e47
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-09 23:54:04 -0300

    88. Ex. 3

---
+++ js2020-course/05-fuctions/exercises/ex88-3.js
@@ -0,0 +1,18 @@
+const power = (base, exponent) => Math.pow(base,exponent)
+
+console.log('1.) ', power(10,3))
+console.log('2.) ', power(10,6))
+console.log('3.) ', power(10,9))
+
+// Answer
+/* function expoente (base, expoente) {
+    //Podemos fazer de 2 modos
+    //Método antigo:
+    let resultado = Math.pow(base, expoente)
+    //Método novo:
+    resultado = base ** expoente
+
+    return resultado
+}
+
+console.log(expoente(2, 3)) */
