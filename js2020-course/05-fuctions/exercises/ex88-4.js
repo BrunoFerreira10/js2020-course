55316fe88b7c733821c6368c32782be207f56e5c
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-10 00:06:09 -0300

    88. Ex. 4

---
+++ js2020-course/05-fuctions/exercises/ex88-4.js
@@ -0,0 +1,15 @@
+const division = (dividend, divider) => {    
+    console.log(`Result: ${Math.floor(dividend / divider)}, Remainder: ${dividend % divider}`)
+}
+
+division(10,3)
+division(10,6)
+division(10,9)
+
+// Answer
+/* function divisao (dividendo, divisor) {
+    console.log("Resultado: " + Math.floor(dividendo/divisor));
+    console.log(`Resto: ${dividendo % divisor}`)
+}
+
+divisao(11, 4) */
