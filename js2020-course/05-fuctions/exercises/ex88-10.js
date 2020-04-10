5971fe6148854b3d4644ea28c4c8013fec3fd628
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-10 11:56:50 -0300

    88. Ex. 10

---
+++ js2020-course/05-fuctions/exercises/ex88-10.js
@@ -0,0 +1,22 @@
+const divisibleBy3 = (number) => !(number % 3)
+
+console.log('1.)', divisibleBy3(6))
+console.log('2.)', divisibleBy3(12))
+console.log('3.)', divisibleBy3(7))
+console.log('4.)', divisibleBy3(14))
+console.log('5.)', divisibleBy3(112))
+
+
+// Answer
+/*function verificaNumero (numero) {
+    if(numero % 3 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(verificaNumero(3))
+console.log(verificaNumero(2))
+console.log(verificaNumero(150))
+*/
