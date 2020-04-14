80bd77e1facda28655804584f91ea8339f967616
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-14 18:39:05 -0300

    128. Solving challenge

--- js2020-course/08-node/employees/employees.js
+++ js2020-course/08-node/employees/employees.js
@@ -1,19 +1,35 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+// My solution
 axios.get(url).then(response => {
     const employees = response.data
     const filterGender = gender => employee => employee.genero === gender
     const filterCountry = country => employee =>  employee.pais === country
     const getLowerSalary = (prev, actual) => actual.salario < prev.salario ? actual : prev
     
-    let youngChineseGirl = employees.filter(filterGender('F'))
-    youngChineseGirl = youngChineseGirl.filter(filterCountry('China'))
-    youngChineseGirl = youngChineseGirl.reduce(getLowerSalary)
+    let lowerSalaryChineseGirl = employees.filter(filterGender('F'))
+    lowerSalaryChineseGirl = lowerSalaryChineseGirl.filter(filterCountry('China'))
+    lowerSalaryChineseGirl = lowerSalaryChineseGirl.reduce(getLowerSalary)
 
     console.log(lowerSalaryChineseGirl)
 })
 
+// Class solution
+const chineses = f => f.pais === 'China'
+const mulheres = f =>  f.genero === 'F'
+const menorSalario = (func, funcAtual) =>  func.salario < funcAtual.salario ? func : funcAtual
+
+axios.get(url).then(response => {
+    const funcionarios = response.data    
+    
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+
+    console.log(func)
+})
 
 
 
