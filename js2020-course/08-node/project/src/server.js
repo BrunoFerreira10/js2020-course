1baa6a6bfc64fa29d1fab55f3fc6941aee0cf44e
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-15 12:46:48 -0300

    140. Project: API Rest with Express #01

---
+++ js2020-course/08-node/project/src/server.js
@@ -0,0 +1,12 @@
+const port = 3003
+
+const express = require('express')
+const app = express()
+
+app.get('/products', (req, res, next) => {
+    res.send({name: 'Notebook', price: 123.45}) // Converts to JSON    
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
