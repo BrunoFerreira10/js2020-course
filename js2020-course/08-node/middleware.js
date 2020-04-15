2263775809b22266b94f5aefa0adc62f43ea1e53
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-15 12:10:31 -0300

    138. Middleware #02

---
+++ js2020-course/08-node/middleware.js
@@ -0,0 +1,39 @@
+// Middleware pattern (chain of responsability)
+
+const step1 = (context, next) => {
+    context.value1 = 'mid1'
+    next()
+}
+
+const step2 = (context, next) => {
+    context.value2 = 'mid2'
+    next()
+}
+
+const step3 = context => context.value3 = 'mid3'
+
+const exec = (context, ...middlewares) => {
+    const execStep = index => {
+        middlewares && 
+        index < middlewares.length && 
+        middlewares[index](context, () => execStep(index + 1))
+    }
+    execStep(0)
+}
+
+
+let context = {}
+exec(context, step1, step2, step3)
+console.log(context)
+
+context = {}
+exec(context, step2, step3)
+console.log(context)
+
+context = {}
+exec(context, step2)
+console.log(context)
+
+context = {}
+exec(context)
+console.log(context)
