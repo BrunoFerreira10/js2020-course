61d348d55c0a75b522095412366c9a027a148ece
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-15 14:27:06 -0300

    Project: API com express #03

--- js2020-course/08-node/project/src/server.js
+++ js2020-course/08-node/project/src/server.js
@@ -2,9 +2,39 @@ const port = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const dataBase = require('./database')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/products', (req, res, next) => {
-    res.send({name: 'Notebook', price: 123.45}) // Converts to JSON    
+    res.send(dataBase.getProducts()) 
+})
+
+app.get('/products/:id', (req, res, next) => {
+    res.send(dataBase.getProduct(req.params.id))
+})
+
+app.post('/products', (req, res, next) => {
+    const product = dataBase.saveProduct({
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product)
+})
+
+app.put('/products/:id', (req, res, next) => {
+    const product = dataBase.saveProduct({
+        id: req.params.id,
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product)
+})
+
+app.delete('/products/:id', (req, res, next) => {
+    const product = dataBase.deleteProduct(req.params.id)
+    res.send(product)
 })
 
 app.listen(port, () => {
