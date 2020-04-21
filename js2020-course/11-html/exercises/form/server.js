3385e12354c50a035b8d288db2ac43e92f2c06bb
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-21 14:04:45 -0300

    185. Forms #03

--- js2020-course/11-html/exercises/form/server.js
+++ js2020-course/11-html/exercises/form/server.js
@@ -7,7 +7,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/users', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Congratulations!</h1>')
+    resp.send('<h1>Congratulations! User created!</h1>')
+})
+
+app.post('/users/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Congratulations! User updated!</h1>')
 })
 
 app.listen(3003)
