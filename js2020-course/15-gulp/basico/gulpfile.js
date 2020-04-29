972f8b41b27a846c4b38c2f50e8b50bfcda98642
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-04-28 21:21:37 -0300

    274. Gulp Basics #02

--- js2020-course/15-gulp/basico/gulpfile.js
+++ js2020-course/15-gulp/basico/gulpfile.js
@@ -1,10 +1,29 @@
 const gulp = require('gulp')
-const series = gulp.series
+const {series, parallel} = require('gulp')
 
-function copy(callback){
-    console.log('Copy task!!!')
+function before1(callback){
+    console.log('Task Before 1!!!')
     return callback()
 }
 
+function before2(callback){
+    console.log('Task Before 2!!!')
+    return callback()
+}
 
-module.exports.default = series(copy)
+function copy(callback){
+    //gulp.src(['pastaA/arquivo1.txt','pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'))
+    return callback()
+}
+
+function end(callback){
+    console.log('Task end!!!')
+    return callback()
+}
+
+module.exports.default = series(
+    parallel(before1,before2),
+    copy,
+    end)
