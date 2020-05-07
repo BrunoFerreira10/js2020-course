278e589b9bee5a232a1d364118c77f0cb36edd50
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-07 11:56:48 -0300

    350. First component

---
+++ js2020-course/20-react/exercises/src/components/First.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// export default function() {
+//     return <h1>First</h1>
+// }
+
+export default () => (
+    <div>
+        <h1>H1</h1>
+        <h2>H2</h2>
+        <h3>H3</h3>
+        <h4>H4</h4>
+        <h5>H5</h5>
+    </div>
+)
--- js2020-course/20-react/exercises/src/index.js
+++ js2020-course/20-react/exercises/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import First from './components/First'
 
-ReactDOM.render(<h1>React</h1>, document.getElementById('root'))
 
-const element = <h2>React 2</h2>
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(<First/>, document.getElementById('root'))
+
