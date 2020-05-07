e6b7e0a89d670583f724964bdc0828523017ff10
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-07 12:20:38 -0300

    352. Multiple components

---
+++ js2020-course/20-react/exercises/src/components/Multiples.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const GoodAfternoon = props => <h1>Good afternoon {props.name}!</h1>
+
+export const GoodNigth = props => <h1>Good nigth {props.name}!</h1>
+
+export default {GoodAfternoon, GoodNigth}
--- js2020-course/20-react/exercises/src/index.js
+++ js2020-course/20-react/exercises/src/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import GoodMorning from './components/GoodMorning'
-
-ReactDOM.render(<GoodMorning name="Guilherme"/>, document.getElementById('root'))
+import Multi, {GoodNigth} from './components/Multiples'
+ 
+ReactDOM.render(
+    <React.Fragment>
+        <GoodMorning name="Guilherme"/>
+        <Multi.GoodAfternoon name="Ana"/>
+        <GoodNigth name="Bia"/>
+    </React.Fragment>
+, document.getElementById('root'))
 
