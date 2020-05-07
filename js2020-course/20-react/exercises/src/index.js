bee025455828f10dba61dc66ea139f400e33b358
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-07 17:20:14 -0300

    356. Parents and Childs components #02

--- js2020-course/20-react/exercises/src/index.js
+++ js2020-course/20-react/exercises/src/index.js
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Parent from './components/Parent'
+import Child from './components/Child'
  
 ReactDOM.render(
     <React.Fragment>
-        <Parent name="Paulo" surname="Silva"/>        
+        <Parent name="Paulo" surname="Silva">       
+            {/* How assign childs components here?*/}
+            <Child name="Pedro" surname="Silva" />
+            <Child name="Paulo" surname="Silva" />
+            <Child name="Carla" surname="Silva" />
+        </Parent>
     </React.Fragment>
 , document.getElementById('root'))
 
