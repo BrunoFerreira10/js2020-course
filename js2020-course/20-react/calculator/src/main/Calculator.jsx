9969e4aea064f8ca4258cd14b3fb2efc55677d05
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-07 20:14:20 -0300

    362. Implementing the logic #02

--- js2020-course/20-react/calculator/src/main/Calculator.jsx
+++ js2020-course/20-react/calculator/src/main/Calculator.jsx
@@ -20,7 +20,7 @@ export default class Calculator extends Component {
         super()
         this.clearMemory = this.clearMemory.bind(this)
         this.setOperation = this.setOperation.bind(this)
-        this.addDigit = this.addDigit.bind(this)
+        this.addDigit = this.addDigit.bind(this)        
     }
 
     clearMemory() {
@@ -29,7 +29,31 @@ export default class Calculator extends Component {
 
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true})
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+            const values = [...this.state.values]
+
+            try {
+                console.log(values, currentOperation)
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)                
+            } catch (error) {
+                values[0] = this.state.values[0]
+            }
+            
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+            
+        }
     }
 
     addDigit(n){
@@ -41,6 +65,14 @@ export default class Calculator extends Component {
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
         this.setState({displayValue, clearDisplay: false})
+
+        if(n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({values})
+        }
         
     }
 
