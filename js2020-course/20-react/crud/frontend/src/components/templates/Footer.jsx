f1961035b7663e74f4bcaaff2418ca836e24188f
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-11 12:35:27 -0300

    370. Component Footer

--- js2020-course/20-react/crud/frontend/src/components/templates/Footer.jsx
+++ js2020-course/20-react/crud/frontend/src/components/templates/Footer.jsx
@@ -3,5 +3,8 @@ import './Footer.css'
 
 export default props => 
     <footer className="footer">
-        Footer
+        <span>
+            Developed with <i className="fa fa-heart text-danger"></i> by
+            <strong> Cod<span className="text-danger">3</span>r</strong> | Implemented by Bruno Ferreira
+        </span>
     </footer>
