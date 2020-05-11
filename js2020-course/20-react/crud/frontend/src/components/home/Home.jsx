01ddd4a45f418d20d0e5ca407ddf2284f245ef82
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-11 13:08:14 -0300

    372. Component Contents

---
+++ js2020-course/20-react/crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props => 
+    <Main icon="home" title="Start" subtitle="Second project of React Chapter" >
+        <div className="display-4">Welcome</div>
+        <hr/>
+        <p className="mb-0">System to exemplify a register made in React.</p>
+    </Main>    
--- js2020-course/20-react/crud/frontend/src/main/App.jsx
+++ js2020-course/20-react/crud/frontend/src/main/App.jsx
@@ -5,13 +5,13 @@ import 'font-awesome/css/font-awesome.min.css'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
-import Main from '../components/templates/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/templates/Footer'
 
 export default props => 
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" title="Start" subtitle="Second project of React Chapter" />
+        <Home />
         <Footer />
     </div>
