0d2f0dce686f217b34e15fc110ab226eba2854a6
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-11 13:32:01 -0300

    374. Implementing Routes(React Routes)

--- js2020-course/20-react/crud/frontend/src/components/templates/Logo.jsx
+++ js2020-course/20-react/crud/frontend/src/components/templates/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Logo.css'
+import { Link } from 'react-router-dom'
 
 import logo from '../../assets/images/logo.png'
 
 export default props => 
     <aside className="logo">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
             <img src={logo} alt="logo"/>
-        </a>
+        </Link>
     </aside>
--- js2020-course/20-react/crud/frontend/src/components/templates/Nav.jsx
+++ js2020-course/20-react/crud/frontend/src/components/templates/Nav.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Nav.css'
+import { Link } from 'react-router-dom'
 
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
             {/* Refactor at home!! */}
-            <a href="#/">
+            <Link to="/">
                 <i className="fa fa-home"></i> Start
-            </a>
-            <a href="#/users">
+            </Link>
+            <Link to="/users">
                 <i className="fa fa-users"></i> Users
-            </a>
+            </Link>
         </nav>
     </aside>
