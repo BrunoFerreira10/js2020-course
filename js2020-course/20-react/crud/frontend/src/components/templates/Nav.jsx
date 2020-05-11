69e960c2ac68a32c4bc10eff72af7ab5f067aa7c
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-11 12:43:56 -0300

    371. Navigation Footer

--- js2020-course/20-react/crud/frontend/src/components/templates/Nav.jsx
+++ js2020-course/20-react/crud/frontend/src/components/templates/Nav.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import './Nav.css'
 
-export default props => 
+export default props =>
     <aside className="menu-area">
-        Navigation
+        <nav className="menu">
+            {/* Refactor at home!! */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Start
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Users
+            </a>
+        </nav>
     </aside>
