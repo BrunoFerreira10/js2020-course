acbaa443968c135fd57bbb154761cca59e840900
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-12 14:12:25 -0300

    397. Implementing logic

--- js2020-course/23-vue/calculator/src/main/Calculator.vue
+++ js2020-course/23-vue/calculator/src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="calculator">
-        <Display />
+        <Display :value="displayValue" />
         <Button label="AC" triple @onCalcButtonClick="clearMemory" />
         <Button label="/" operation @onCalcButtonClick="setOperation" />
         <Button label="7" @onCalcButtonClick="addDigit" />
@@ -26,16 +26,61 @@ import Display from '../components/Display'
 import Button from '../components/Button'
 
 export default {
+    data: function() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0,0],
+            current: 0
+        }
+    },
     components: { Button, Display },
     methods: {
         clearMemory() {
-            
+            Object.assign(this.$data, this.$options.data())            
         },
         setOperation(operation) {
+            if(this.current === 0) {
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === "="
+                const currentOperation = this.operation
 
+                try {
+                    this.values[0] = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                    )
+                } catch (error) {
+                    this.$emit('onError', error)
+                }
+                
+                this.values[1] = 0
+
+                this.displayValue = this.values[0]
+                this.operaton = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+            }
         },
         addDigit(n){
+            if(n === '.' && this.displayValue.includes('.'))
+                return
 
+            const clearDisplay = this.displayValue === "0" || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+            this.displayValue = displayValue
+            this.clearDisplay = false
+
+            /* if (n !== '.') {
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.values[i] = newValue
+            } */
+            this.values[this.current] = displayValue
         }
     }
 }
