3f350bad2f978273f2f507ccc47d359705626552
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-14 18:38:53 -0300

    433. Product tables

--- js2020-course/27-angular/crud/frontend/src/app/app.module.ts
+++ js2020-course/27-angular/crud/frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,6 +29,11 @@ import { ProductListSchemaComponent } from './components/product/product-list-sc
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort'
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
@@ -61,7 +66,12 @@ import { MatSortModule } from '@angular/material/sort'
     MatPaginatorModule,
     MatSortModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
