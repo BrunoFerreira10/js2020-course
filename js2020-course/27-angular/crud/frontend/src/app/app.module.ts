e9e430ac9d6e4e724c7a662574bbb25799cd8de2
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-14 00:01:59 -0300

    421. Fixed backgrounds

--- js2020-course/27-angular/crud/frontend/src/app/app.module.ts
+++ js2020-course/27-angular/crud/frontend/src/app/app.module.ts
@@ -16,7 +16,6 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
-import { ForDirective } from './directives/for.directive'
 
 @NgModule({
   declarations: [
@@ -26,8 +25,7 @@ import { ForDirective } from './directives/for.directive'
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective,
-    ForDirective
+    RedDirective
   ],
   imports: [
     BrowserModule,
