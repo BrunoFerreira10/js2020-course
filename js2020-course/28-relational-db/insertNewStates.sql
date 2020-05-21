6d2695425f85e45feaecc2bbdd162912275eae4e
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-21 18:40:23 -0300

    456. Inserting states with id

---
+++ js2020-course/28-relational-db/insertNewStates.sql
@@ -0,0 +1,10 @@
+insert into states (id, name, initials, region, population)
+values
+  (100, 'New', 'NV', 'South', 2.54);
+
+select * from states;
+
+insert into states (name, initials, region, population)
+values
+  ('More New', 'MN', 'North', 2.51);
+
