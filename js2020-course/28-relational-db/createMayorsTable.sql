eade1cc522d1462f0becb62d2f9fcde2891a3cbc
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-21 20:51:03 -0300

    464. Insert Mayors

--- js2020-course/28-relational-db/createMayorsTable.sql
+++ js2020-course/28-relational-db/createMayorsTable.sql
@@ -1,7 +1,7 @@
 create table if not exists mayors (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    cities_id INT UNSIGNED NOT NULL,
+    cities_id INT UNSIGNED,
     PRIMARY KEY (id),
     unique key (cities_id),
     FOREIGN KEY (cities_id) REFERENCES cities (id)
---
+++ js2020-course/28-relational-db/insertMayors.sql
@@ -0,0 +1,11 @@
+insert into mayors
+    (name, cities_id)
+values
+    ('Rodrigo Neves', 2),
+    ('Raquel Lyra', 3),
+    ('Zenaldo Coutinho', null);
+
+insert into mayors
+    (name, cities_id)
+values
+    ('Rafael Greca', null);
