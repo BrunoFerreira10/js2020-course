5e8bf6363e49587e118c7a38032c7488721df2be
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-05-21 18:10:40 -0300

    453. Inserting states

---
+++ js2020-course/28-relational-db/createStatesTable.sql
@@ -0,0 +1,12 @@
+-- Creating state table!
+
+create table states (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+    initials VARCHAR(2) NOT NULL,
+    region ENUM('North', 'Northeast', 'Midwest', 'Southeast', 'South') NOT NULL,
+    population DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (name),
+    UNIQUE KEY (initials)
+);
