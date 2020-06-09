d7eb48bbd376f8f6c5f9d388cc26b7535a7a16f5
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-06-08 21:21:41 -0300

    502. Express - Handling the fist request

--- js2020-course/31-express/index.js
+++ js2020-course/31-express/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 
 const app = express()
 
+
+app.get('/opa',(req, res) => {
+    res.send("I'm fine too!")
+})
+
 app.listen(3000, () => {
     console.log('Backend running...')
 })
