41463c2005248fbea05f4827873fec6deaabd03b
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-06-11 15:10:14 -0300

    520. Final project backend - User API

--- js2020-course/32-final-project/versao-inicial/backend/api/user.js
+++ js2020-course/32-final-project/versao-inicial/backend/api/user.js
@@ -1,9 +1,62 @@
+const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
-    const save = (req, res) => {
-        res.send('User save')
+    const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
+
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
+    }
+
+    const save = async (req, res) => {
+        const user = {...req.body}
+
+        if(req.params.id)
+            user.id = req.params.id
+
+        try {
+            existsOrError(user.name, "Name not provided.")
+            existsOrError(user.email, "E-Mail not provided.")
+            existsOrError(user.password, "Password not provided.")
+            existsOrError(user.confirmPassword, "Password confirmation not provided.")
+            equalsOrError(user.password, user.confirmPassword, "Passwords do not match.")
+
+            const userFromDb = await app.db('users')
+                                    .where({email: user.email}).first()
+
+            if(!user.id){
+                notExistsOrError(userFromDb, 'User already registred.')
+            }            
+        } catch (msg) {
+            return res.status(400).send(msg)
+        }
+
+        user.password = encryptPassword(user.password)
+        delete user.confirmPassword
+
+        if(user.id) {
+            app.db('users')
+                .update(user)
+                .where({id: user.id})
+                .then( _ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('users')
+                .insert(user)
+                .then( _ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
+    }
+
+    const get = (req, res) => {
+        app.db('users')
+            .select('id', 'name', 'email', 'admin')
+            .then(users => res.json(users))
+            .catch(err => res.status(500).send(err))
     }
 
     return {
-        save
+        save,
+        get
     }
 }
--- js2020-course/32-final-project/versao-inicial/backend/config/routes.js
+++ js2020-course/32-final-project/versao-inicial/backend/config/routes.js
@@ -13,4 +13,8 @@ module.exports = app => {
 module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
+        .get(app.api.user.get)
+
+        app.route('/users/:id')
+            .put(app.api.user.save)            
 }
