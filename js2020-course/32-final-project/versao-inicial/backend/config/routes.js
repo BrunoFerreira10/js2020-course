23f658f855cc46736230d9800a1bcdb479e57e92
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-06-11 16:44:21 -0300

    522. Final project backend - Category API #01

--- js2020-course/32-final-project/versao-inicial/backend/config/routes.js
+++ js2020-course/32-final-project/versao-inicial/backend/config/routes.js
@@ -7,7 +7,7 @@ module.exports = app => {
          **** OR ****
         .post(user.save)
 } */
- 
+
 // Method not using consign
 
 module.exports = app => {
@@ -18,5 +18,15 @@ module.exports = app => {
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.get)
-                        
+
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+
+
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
+
 }
