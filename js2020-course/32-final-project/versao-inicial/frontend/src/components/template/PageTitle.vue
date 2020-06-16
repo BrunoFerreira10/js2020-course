4adc847bd6836493ca13f74403a7da32cd0a6122
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-06-16 19:56:08 -0300

    539. Final project frontend - Page title component

---
+++ js2020-course/32-final-project/versao-inicial/frontend/src/components/template/PageTitle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page-title">
+    <h1>
+      <i v-if="icon" :class="icon"/> {{ title }}
+    </h1>
+    <h2>
+      {{ subtitle }}
+    </h2>
+    <hr>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageTitle',
+  props: ['icon', 'title', 'subtitle']
+}
+</script>
+
+<style>
+  .page-title h1 {
+    margin: 0;
+  }
+
+  .page-title h2 {
+    color: #777;
+    margin-top: 5px;
+    font-size: 1.3rem;
+  }
+  
+</style>
