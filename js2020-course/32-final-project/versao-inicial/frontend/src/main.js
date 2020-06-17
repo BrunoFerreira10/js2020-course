5e0686e7d1e490bd35cbdf47ab503e301a98fb63
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-06-16 21:14:31 -0300

    541. Final project frontend - Component Home

--- js2020-course/32-final-project/versao-inicial/frontend/src/main.js
+++ js2020-course/32-final-project/versao-inicial/frontend/src/main.js
@@ -9,6 +9,10 @@ import router from  './config/router'
 
 Vue.config.productionTip = false
 
+// TEMP LOGIN
+
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkJydW5vIEZlcnJlaXJhIiwiZW1haWwiOiJicnVub2ZlcnJlaXJhODZtYWluQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1OTIzNTE5ODcsImV4cCI6MTU5MjYxMTE4N30.seDmdlMbZ3kvsCYhvFukxo5sJR6gF-FUmFOYtv0ptaA'
+
 new Vue({
   store,
   router,
